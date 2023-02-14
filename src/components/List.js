0fd1a65be75b82cfc5a43147d371ae146ae263a8
github-marynapoliarush <merienf8118@gmail.com>
2023-02-14 11:00:47 +0200

    add styling

--- src/components/List.js
+++ src/components/List.js
@@ -7,10 +7,19 @@ const List = (props)=>{
         <div className='ul_wrapper'>
             <div className="ul_header">
                 <h3>Available Apartments (3)</h3>
-                <select name="select"> 
-                    <option value="value1">Price: lowest to highest</option>
-                    <option value="value2" selected>Price: highest to lowest</option>
-                </select>
+                <div className='filters'>
+                    <div className='roomsFilter'>
+                        <span>Filter by rooms</span>
+                        <input type='number' min='1' defaultValue='1'/>
+                    </div>
+                    <div className='priceFilter'>
+                        <span>Order by</span>
+                        <select name="select"> 
+                            <option value="value1">Price: lowest to highest</option>
+                            <option value="value2" selected>Price: highest to lowest</option>
+                        </select>
+                    </div>
+                </div>                             
             </div>
         
             <ul className="">
